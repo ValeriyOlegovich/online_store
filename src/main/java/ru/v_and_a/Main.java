b1279eb1d662e714f872c99791933bad4a92da7a
Валерий <valera.kurzyakov@yandex.ru>
2026-04-08 15:31:18 +0300

    first commit

---
+++ src/main/java/ru/v_and_a/Main.java
@@ -0,0 +1,8 @@
+package ru.v_and_a;
+
+//TIP Для <b>запуска</b> кода нажмите <shortcut actionId="Run"/> или
+// щелкните значок <icon src="AllIcons.Actions.Execute"/> в боковой области.
+public class Main {
+    public static void main(String[] args) {
+    }
+}
