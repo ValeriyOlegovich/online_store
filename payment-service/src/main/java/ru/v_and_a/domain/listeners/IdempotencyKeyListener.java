d629b073ef7e949e97be8de34bd58fa1f4de30a3
Валерий <valera.kurzyakov@yandex.ru>
2026-04-23 14:05:09 +0300

    оптимизация пропертей

--- payment-service/src/main/java/ru/v_and_a/domain/listeners/IdempotencyKeyListener.java
+++ payment-service/src/main/java/ru/v_and_a/domain/listeners/IdempotencyKeyListener.java
@@ -1,11 +1,12 @@
 package ru.v_and_a.domain.listeners;
 
 import jakarta.persistence.PreRemove;
+import ru.v_and_a.domain.model.IdempotencyKey;
 
 public class IdempotencyKeyListener {
 
     @PreRemove
-    public void preRemove() {
+    public void preRemove(IdempotencyKey key) {
         throw new IllegalStateException("Удаление записей из IdempotencyKey запрещено!");
     }
 }
